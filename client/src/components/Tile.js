cb6fb5ec211dbe6de632fb7b1b81603faea9350d
innessa89 <innessa89@gmail.com>
2021-12-03 22:00:29 +0000

    gem and bomb sound effects

--- client/src/components/Tile.js
+++ client/src/components/Tile.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./static/Tile.css";
 import bombImage from "../assets/bomb.svg";
 import gemImage from "../assets/gem.svg";
+import useSound from 'use-sound';
+import coinSound from "./static/magic.wav";
+import bombSound from "./static/gun.wav";
 
 const Tile = ({tile, index, setClicked, incrementScore}) => {
     
+    const [play]=useSound(coinSound);
+    const[play2]=useSound(bombSound)
+
     const handleClick = function(event) {
         console.log("event:", event.target);
         console.log("event:", event.target.title);
@@ -12,19 +18,22 @@ const Tile = ({tile, index, setClicked, incrementScore}) => {
         const id = event.target.id;
         if (!tile.clicked) {
             setClicked(id);
+            play();
             if (event.target.title === "false") {
-                incrementScore();
-            } 
-        }
+                incrementScore();  
+            } else{
+                play2()
+            }
+        }  
     }
  
+
     return(
             <div id={index} title={tile.value.toString()} className="individual_tile"  onClick={handleClick} style={{border: "1px solid black"}}>
                 {tile.clicked && !tile.value ? <img src={gemImage} className="gem-image"/> : null}
                 {tile.clicked && tile.value ? <img src={bombImage} className="bomb-image"/> : null}
             </div>
     )
-
 };
 
 export default Tile;
