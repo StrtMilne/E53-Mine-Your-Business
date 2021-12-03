d67e36e7908635a0490a6517b446f0523fd8b86a
StrtMilne <oober@hotmail.co.uk>
2021-12-03 18:02:48 +0000

    created a get route within server.js to post db to api

--- server/db/game.sql
+++ server/db/game.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS high_scores;
 
 CREATE TABLE high_scores (
-    id SERIAL PRIMARY KEY,
     player_name VARCHAR(255),
-    score INT
-)
+    score INT,
+    id SERIAL PRIMARY KEY
+);
+
+ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
--- server/server.js
+++ server/server.js
@@ -12,9 +12,25 @@ const client = new Client ({
     "password": "password",
     "host": "localhost",
     "database": "game"
-
 })
 
 client.connect()
-.then(() => console.log("connected successfully"))
-.catch(console.err);
+    .then(() => {
+        console.log("connected successfully")
+    })
+    .catch(console.err);
+
+app.get("/scores", (req, res) => {
+    client.query("SELECT * FROM high_scores;")
+    .then(results => res.send(results.rows));
+})
+
+// WORKING ON
+
+// app.post("/scores", (req, res) => {
+//     let p_name = req.body["player_name"];
+//     let p_score = req.body["score"];
+//     client.query(`INSERT INTO high_scores (player_name, score) VALUES (${p_name}, ${p_score});`)
+// })
+
+app.listen(5000, () => console.log(`Listening on port`));
